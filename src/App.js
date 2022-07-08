ebdb4259c34469d68df547791df6b5c82043c4f3
shaheerinit <105648279+shaheersystems@users.noreply.github.com>
2022-07-08 21:44:59 +0500

    erased

--- src/App.js
+++ src/App.js
@@ -1,16 +1,6 @@
 import "./App.css";
-import Navbar from "./components/Navbar";
-import Wrapper from "./components/Wrapper";
-import Banner from "./components/Banner";
 function App() {
-  return (
-    <div className="App">
-      <Navbar />
-      <Wrapper>
-        <Banner />
-      </Wrapper>
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
